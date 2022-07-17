e00ba5a5271e7cb47d8738ec866c006d2928be47
Andy2472 <89529008+Andy2472@users.noreply.github.com>
2022-07-17 18:19:44 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,10 @@
+function saludar (){
+pregunta=prompt("¿Hola como estás sapo perro?");
+if (pregunta == "Bien" || pregunta == "bien"){
+    alert  ("Que bueno mi pez") 
+} 
+else {
+    alert ("Cha wueba maní")
+}
+}
+saludar ()
